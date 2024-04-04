fe47ec569fd53fc4a307bd65801a268c63bf3dec
Samtav007 <samtav0821@gmail.com>
2024-04-04 11:12:01 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+import elephant from "./images/elephant.jpeg";
+
+const imageData = () => {
+  let data = [
+    {
+      id: 1,
+      img: elephant
+    },
+    {
+      id: 2,
+      img: elephant
+    },
+    {
+      id: 3,
+      img: elephant
+    },
+    {
+      id: 4,
+      img: elephant
+    }
+  ]
+  return data;
+}
+
+function App() {
+  const images = imageData();
+
+  return (
+    <div className="App">
+      <h1>Kalvium gallary</h1>
+      <div className="row">
+        {images.map((image) => (
+          <img key={image.id} src={image.img} alt={`Image ${image.id}`} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
